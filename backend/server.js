04d157eba3d65b24693aa6052683ad20a50a1b6b
tgbao2110 <tranb6778@gmail.com>
2025-06-09 16:50:23 +0700

    be: install backend packages and set up MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import express from "express";
+
+dotenv.config()
+
+//create db connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MONGODB CONNECTED SUCCESSFULLY"))
+  .catch((error) => console.error("ERROR CONNECTING TO MONGODB: ", error));
+
+const app = express();
+const port = process.env.PORT || "5001";
+
+//Middlewares
+app.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET','POST',"PUT",'DELETE'],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true
+    })
+);
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.listen(port, () => {console.log("Server is running on port ",port)})
