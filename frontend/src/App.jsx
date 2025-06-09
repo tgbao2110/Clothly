33fbd9d39e39e87246c3fa11ec7e1b369147c60c
tgbao2110 <tranb6778@gmail.com>
2025-06-09 16:55:49 +0700

    fe: install frontend packages and set up Redux Toolkit store

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <h1 className="text-red-500 font-extrabold self-center">HELLOOOO</h1>
+      <Button className="self-center">WORLD</Button>
+    </div>
+  );
+}
+
+export default App;
