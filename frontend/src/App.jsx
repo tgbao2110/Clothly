f6ba795d871c052eaf01ca3d4f181f633ccd3d5c
tgbao2110 <tranb6778@gmail.com>
2025-06-09 18:00:44 +0700

    fe: set up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/_auth/layout";
+import Login from "./pages/_auth/login";
+import Register from "./pages/_auth/register";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1 className="text-red-500 font-extrabold self-center">HELLOOOO</h1>
-      <Button className="self-center">WORLD</Button>
+      <h1>Header</h1>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout/>}>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "./index.css";
@@ -8,8 +9,10 @@ import store from "./store/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
