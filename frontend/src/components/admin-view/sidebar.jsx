587db403cda186e9a89837acc22d7c5ce3da7531
tgbao2110 <tranb6778@gmail.com>
2025-06-12 22:19:20 +0700

    fe: add admin sidebar menu items

--- frontend/src/components/admin-view/sidebar.jsx
+++ frontend/src/components/admin-view/sidebar.jsx
@@ -1,8 +1,12 @@
 import { ChartNoAxesCombined } from "lucide-react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
+
+import { adminSidebarMenuItems } from '@/config'
 
 const AdminSidebar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <aside className="hidden lg:flex flex-col w-64 border-r bg-background p-6">
 
@@ -16,7 +20,28 @@ const AdminSidebar = () => {
       </div>
 
       {/* ==== Menu Items ==== */}
-
+      <nav>
+        <ul className="mt-7 text-lg ">
+        {
+          adminSidebarMenuItems.map((item, id) => {
+            const isActive = location.pathname.includes(item.path); // set active based on location
+            return(
+              <li
+                key={id} 
+                onClick={()=>navigate(item.path)}
+                className={`flex items-center h-10 px-5 py-7 gap-2 rounded-md hover:bg-accent 
+                cursor-pointer text-muted-foreground hover:text-primary
+                ${ isActive && 'text-primary' }
+                `}
+              >
+                {item.icon}
+                {item.label}
+              </li>
+            )
+          })
+        }
+        </ul>
+      </nav>
     </aside>
   )
 }
--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,3 +1,10 @@
+import { GalleryVertical, Shirt, ShoppingCart, SquareKanban } from "lucide-react";
+//
+//
+//
+// AUTHENTICATION
+//
+//// Register
 export const registerFormControls = [
   {
     name: "userName",
@@ -21,7 +28,8 @@ export const registerFormControls = [
     type: "password",
   },
 ];
-
+//
+//// Login
 export const loginFormControls = [
   {
     name: "email",
@@ -36,6 +44,38 @@ export const loginFormControls = [
     placeholder: "Enter password",
     componentType: "input",
     type: "password",
+  },
+];
+//
+//
+//
+// ADMIN
+//
+//// Admin Sidebar
+export const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <SquareKanban size={18}/>
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <Shirt size={18}/>
+  },
+  {
+    id: "features",
+    label: "Features",
+    path: "/admin/features",
+    icon: <GalleryVertical size={18}/>
+  },
+ {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <ShoppingCart size={18}/>
   },
 ];
 
