4dcadf4af9c6e622c3d54b084d69ae2abaf64a1f
tgbao2110 <tranb6778@gmail.com>
2025-06-12 21:21:22 +0700

    fe: add admin header and sidebar

--- frontend/src/components/admin-view/header.jsx
+++ frontend/src/components/admin-view/header.jsx
@@ -1,6 +1,25 @@
+import { Button } from "../ui/button"
+import { LogOut, Menu } from "lucide-react"
+
 const AdminHeader = () => {
   return (
-    <div>AdminHeader</div>
+    <header className="flex justify-between items-center px-4 py-4 bg-background border-b">
+
+      {/* ==== Toggle Menu ==== */}
+      <Button className="lg:hidden sm:block">
+        <Menu/>
+        <span className="sr-only">Menu</span>
+      </Button>
+
+      {/* ======= Logout ======= */}
+      <div className="flex flex-1 justify-end">
+        <Button >
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+
+    </header>
   )
 }
 export default AdminHeader
--- frontend/src/components/admin-view/sidebar.jsx
+++ frontend/src/components/admin-view/sidebar.jsx
@@ -1,6 +1,23 @@
+import { ChartNoAxesCombined } from "lucide-react"
+import { useNavigate } from "react-router-dom"
+
 const AdminSidebar = () => {
+  const navigate = useNavigate();
   return (
-    <div>AdminSidebar</div>
+    <aside className="hidden lg:flex flex-col w-64 border-r bg-background p-6">
+
+      {/* ==== Title ==== */}
+      <div 
+        className="flex items-center gap-2 cursor-pointer"
+        onClick={() => navigate("/admin/dashboard")}
+      >
+        <ChartNoAxesCombined />
+        <h1 className="text-xl font-extrabold">Clothly Admin</h1>
+      </div>
+
+      {/* ==== Menu Items ==== */}
+
+    </aside>
   )
 }
 export default AdminSidebar
