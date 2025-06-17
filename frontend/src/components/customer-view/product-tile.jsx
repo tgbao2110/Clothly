885cbb130218614e43827b4ac08556ba68a92e6b
tgbao2110 <tranb6778@gmail.com>
2025-06-17 14:18:39 +0700

    fe: add customer product tile component

---
+++ frontend/src/components/customer-view/product-tile.jsx
@@ -0,0 +1,54 @@
+import { Card, CardContent, CardFooter } from "../ui/card"
+
+const CustomerProductTile = ({product}) => {
+
+  return (
+    <Card className="w-full max-w-sm mx-auto pt-0">
+      <div>
+        {/* ===== Image ===== */}
+        <img
+          src={product.image}
+          alt={product.title}
+          className="w-full h-[300px] mb-2 block object-cover"
+        />
+
+        <CardContent>
+          {/* ===== Title ===== */}
+          <h2 className="text-xl font-bold mb-3 truncate">{product.title}</h2>
+
+          {/* ===== Price ===== */}
+          <div className="flex flex-row gap-2 mb-3">
+            <span
+              className={`mb-1 text-md 
+                        ${
+                          product.salePrice !== product.price
+                            ? "line-through text-muted-foreground"
+                            : "font-bold text-primary"
+                        }`}
+            >
+              {"$" + product.price}
+            </span>
+            <span
+              className={`text-md font-bold 
+                    ${product.price === product.salePrice && "invisible"}`}
+            >
+              {product.price !== product.salePrice ? "$" + product.salePrice : "\u00A0" }
+            </span>
+          </div>
+        </CardContent>
+
+        <CardFooter className=" flex flex-row justify-between items-center">
+          {/* ===== Stock ===== */}
+          {product.stock > 0 ? (
+            <div className="text-sm text-muted-foreground">
+              In stock: {product.stock}
+            </div>
+          ) : (
+            <div className="text-sm text-destructive">Out of stock</div>
+          )}
+        </CardFooter>
+      </div>
+    </Card>
+  );
+}
+export default CustomerProductTile
--- frontend/src/pages/customer-view/listing.jsx
+++ frontend/src/pages/customer-view/listing.jsx
@@ -1,4 +1,5 @@
 import ProductFilter from "@/components/customer-view/filter"
+import CustomerProductTile from "@/components/customer-view/product-tile"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Separator } from "@/components/ui/separator"
@@ -9,7 +10,7 @@ const Listing = () => {
   return (
     <div className="w-full p-4 md:p-6 grid grid-cols-[auto_1fr] gap-3">
       {/* ====== Left Grid (filter) ====== */}
-      <ProductFilter/>
+      <ProductFilter />
 
       {/* ====== Right Grid (content) ====== */}
       <div className="space-y-4">
@@ -17,35 +18,27 @@ const Listing = () => {
           {/* ==== Sort ==== */}
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant='muted' className="hover:bg-accent">
+              <Button variant="muted" className="hover:bg-accent">
                 <ArrowDownUp />
                 Sort by
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className='mr-6'>
-              {
-                sortOptions.map(option => (
-                  <DropdownMenuItem>
-                    {option.label}
-                  </DropdownMenuItem>
-                ))
-              }
+            <DropdownMenuContent className="mr-6">
+              {sortOptions.map((option) => (
+                <DropdownMenuItem>{option.label}</DropdownMenuItem>
+              ))}
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
 
-        <Separator className='mb-8'/>
+        <Separator className="mb-8" />
 
         {/* ==== Products ==== */}
-        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
-              <div>Product</div>
-              <div>Product</div>
-              <div>Product</div>
-              <div>Product</div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+         
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 export default Listing
