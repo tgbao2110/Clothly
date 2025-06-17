932b2b1997c350a10227801112d2c7db1d2febbb
tgbao2110 <tranb6778@gmail.com>
2025-06-17 23:41:44 +0700

    be: handle filter products by request query string

--- backend/controllers/customer/products-controller.js
+++ backend/controllers/customer/products-controller.js
@@ -2,7 +2,26 @@ import {Product} from '../../models/Product.js'
 
 const getFilteredProducts = async(req, res) => {
     try {
-        const products = await Product.find({})
+        const query = {};
+        // Expected:
+        // query = { category: { $in: ['men', 'women'] },
+        //           brand: { $in: ['nike', 'adidas'] }, }
+
+
+        // Parse categories from req.query
+        if (req.query.categories) {
+            const categories = req.query.categories.split(',');
+            query.category = { $in: categories };
+        }
+
+        // Parse brands from req.query
+        if (req.query.brands) {
+            const brands = req.query.brands.split(',');
+            query.brand = { $in: brands };
+        }
+
+        // Find products by query
+        const products = await Product.find(query);
 
         res.status(200).json({
             success: true,
