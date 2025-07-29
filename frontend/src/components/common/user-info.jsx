3abf40e1f9b9f221eee17b24e94bf1ff2db1e649
tgbao2110 <tranb6778@gmail.com>
2025-07-29 16:36:47 +0700

    fe: add hide avatar option for use-info

--- frontend/src/components/common/user-info.jsx
+++ frontend/src/components/common/user-info.jsx
@@ -1,13 +1,16 @@
 import { Avatar, AvatarFallback } from "../ui/avatar"
 
-const UserInfo = ({user}) => {
+const UserInfo = ({user, hideAvatar = false}) => {
   return (
     <div className="flex flex-row items-center gap-1.5">
-      <Avatar className='size-10'>
-        <AvatarFallback className="bg-black text-white font-medium">
-          {user?.userName[0]?.toUpperCase() || 'U'}
-        </AvatarFallback>
-      </Avatar>
+      {
+        !hideAvatar &&
+        <Avatar className='size-10'>
+          <AvatarFallback className="bg-black text-white font-medium">
+            {user?.userName[0]?.toUpperCase() || 'U'}
+          </AvatarFallback>
+        </Avatar>
+      }
       <div className="flex flex-col justify-center">
         <h2 className="text-md font-bold">
           {user?.userName || 'User'}
